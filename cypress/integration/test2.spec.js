4df492f54aa5aeec1bcb75c6677ef252b44bdbbf
Tatsiana Astrouskaya <82601614+tattyola@users.noreply.github.com>
2021-12-25 23:19:19 -0500

    add test2.spec.js (#2)

---
+++ cypress/integration/test2.spec.js
@@ -0,0 +1,20 @@
+/// <reference types="Cypress" />
+
+describe('My second test', () => {
+    it('Test case', () => {
+        cy.visit('/');
+        cy.get('.search-keyword').type('ca');
+        cy.wait(2000);
+        cy.get('.products').as('productLocator');
+        cy.get('@productLocator').find('.product').each(($el, index, $list) => {
+            const textVeg = $el.find('.product-name ').text();
+            if (textVeg.includes('Cashews')) {
+                cy.wrap($el).find('button').click();
+            }
+        })
+        cy.get('.cart-icon > img').click();
+        cy.contains('PROCEED TO CHECKOUT').click();
+        cy.contains('Place Order').click();
+
+    })
+})
