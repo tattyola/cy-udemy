bcff0ee71ed2b692461031ba9bea6ba32102b94a
Tatsiana A <astrouskaya@yahoo.com>
2021-12-25 23:19:02 -0500

    add test2.spec.js

--- cypress/integration/test1.spec.js
+++ cypress/integration/test1.spec.js
@@ -5,18 +5,27 @@ describe('My first test', () => {
         cy.visit('/');
         cy.get('.search-keyword').type('ca');
         cy.wait(2000);
-        //cy.get('div[class="product"]').should('have.length', 4);
-        //cy.get('.product:visible').should('have.length', 4);
-        cy.get('.products').find('.product').should('have.length', 4);
-        cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click();
-        cy.get('.products').find('.product').each(($el, index, $list) => {
+        // cy.get('div[class="product"]').should('have.length', 4);
+        // cy.get('.product:visible').should('have.length', 4);
+        // parent child chaining
+        cy.get('.products').as('productLocator');
+        cy.get('@productLocator').find('.product').should('have.length', 4);
+        cy.get('@productLocator').find('.product').eq(2).contains('ADD TO CART').click().then(function() {
+            console.log('sf')
+        })
+        // cy.get('@productLocator').find('.product').eq(2).contains('ADD TO CART').click();
+        // console.log('sf'); // it will print out firs in console coz cypress is not async and coz console.log is a not cypress method
+        cy.get('@productLocator').find('.product').each(($el, index, $list) => {
             const textVeg = $el.find('.product-name ').text();
             if (textVeg.includes('Cashews')) {
                 cy.wrap($el).find('button').click();
             }
         })
+        // assert if logo text is correctly displayed
+        cy.get('.brand').should('have.text', 'GREENKART')
+        // this is to print in logs
         cy.get('.brand').then(function (logoElement) {
-            cy.log(logoElement.text());
+            cy.log(logoElement.text());// it is cypress method that is why it would be print out async
         })
         //const logo = cy.get(.brand) we can't do that, we have to use .than() to make it async
         //cy.log(logo.text())
